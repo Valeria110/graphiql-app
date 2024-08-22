3fbab163ac497213f42296f991fb8bfb4848b63a
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-08-22 14:45:59 +0200

    feat: add simple color & font custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import '../styles/globalStyles.scss';
 import Footer from '@/components/Footer/Footer';
+import { CustomThemeProvider } from '@/components/CustomThemeProvider.tsx/CustomThemeProvider';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] });
 
@@ -20,9 +21,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <Header />
-        <ErrorBoundary>{children}</ErrorBoundary>
-        <Footer />
+        <CustomThemeProvider>
+          <Header />
+          <ErrorBoundary>{children}</ErrorBoundary>
+          <Footer />
+        </CustomThemeProvider>
       </body>
     </html>
   );
