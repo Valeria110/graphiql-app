da4d13982dd1ece15ee717bdcdada0aca3d8a41e
Viktoria Rashchepkina <vika.rashchepkina.04@bk.ru>
2024-08-23 11:25:42 +0500

    feat: add section about course

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -4,11 +4,23 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import welcomePageStyles from './welcomePageStyles.module.scss';
 import restLogo from '../../assets/rest-api-logo.png';
 import graphQLLogo from '../../assets/graphQL-logo.png';
+import rssLogo from '../../assets/svg/rs-school.svg';
 import Image from 'next/image';
 import { logInWithEmailAndPassword } from '@/authService';
 import { customUserName } from '@/utils/customNameUser';
 import Link from 'next/link';
 import { TeamList } from '@/components/TeamList/TeamList';
+
+const courseDescription = [
+  'Main features of the course:',
+  '1. Deep learning of React',
+  '2. Project-oriented approach',
+  '3. Mentoring and code review',
+  '4. Support for modern tools',
+  '5. Community and Career Opportunities',
+  '',
+  "RS School's React course is a great opportunity for developers to deepen their knowledge and skills by creating modern web applications using one of the most in-demand tools on the market.",
+];
 
 function WelcomePage() {
   const [user, loading] = useAuthState(auth);
@@ -62,7 +74,7 @@ function WelcomePage() {
         </div>
         <div className={welcomePageStyles['section-welcome__logos-container']}>
           <Image
-            className={welcomePageStyles['section-welcome__img']}
+            className={welcomePageStyles['section-welcome__logo']}
             src={restLogo}
             alt="logo"
             width={230}
@@ -71,7 +83,7 @@ function WelcomePage() {
           />
           +
           <Image
-            className={welcomePageStyles['section-welcome__img']}
+            className={welcomePageStyles['section-welcome__logo']}
             src={graphQLLogo}
             alt="logo"
             width={200}
@@ -81,9 +93,33 @@ function WelcomePage() {
         </div>
       </section>
       <section className={welcomePageStyles['section-team']}>
-        <h2 className={welcomePageStyles['section-team__title']}>Our team</h2>
+        <h2 className={welcomePageStyles['section-team__title']}>Our Team</h2>
         <h3 className={welcomePageStyles['section-team__subtitle']}>Jumping Skunk</h3>
         <TeamList />
+      </section>
+      <section className={welcomePageStyles['section-course']}>
+        <h2 className={welcomePageStyles['section-course__title']}>Our Learning Path</h2>
+        <h3
+          className={welcomePageStyles['section-course__subtitle']}
+        >{`RS School's React course is an advanced educational program aimed at developers who want to master React and become professional front-end developers. `}</h3>
+        <div className={welcomePageStyles['section-course__description-container']}>
+          <p className={welcomePageStyles['section-course__description']}>
+            {courseDescription.map((line, index) => (
+              <div key={index}>
+                {line}
+                <br />
+              </div>
+            ))}
+          </p>
+          <Image
+            className={welcomePageStyles['section-course__logo']}
+            src={rssLogo}
+            alt="rss-logo"
+            width={200}
+            height={220}
+            priority
+          />
+        </div>
       </section>
     </div>
   );
