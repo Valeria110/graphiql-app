ec07dc95697b2162721655121ed4bbff5b5b1c65
Mikhail Semianiuk <grey.strelok@gmail.com>
2024-08-23 13:13:50 +0200

    fix: name problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import ErrorBoundary from '@/components/ErrorBoundary/ErrorBoundary';
-import Header from '@/components/Header/Header';
+// import Header from '@/components/Header/Header';
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import '../styles/globalStyles.scss';
 import Footer from '@/components/Footer/Footer';
-import { CustomThemeProvider } from '@/components/CustomThemeProvider.tsx/CustomThemeProvider';
+import { CustomThemeProvider } from '@/components/CustomThemeProvider/CustomThemeProvider';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700'] });
 
@@ -12,6 +12,8 @@ export const metadata: Metadata = {
   title: 'Graphiql App',
   description: 'The application which is a light-weight versions of Postman and GraphiQL combined in one app. ',
 };
+
+// TODO: return header
 
 export default function RootLayout({
   children,
@@ -22,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <CustomThemeProvider>
-          <Header />
+          {/* <Header /> */}
           <ErrorBoundary>{children}</ErrorBoundary>
           <Footer />
         </CustomThemeProvider>
