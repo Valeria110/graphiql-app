024d7672f780832e28b8464530922d87ea081127
Valerie Ivashkevich <ivashkevich03@gmail.com>
2024-08-24 11:32:55 +0300

    refactor: format code

--- src/hooks/authHook.ts
+++ src/hooks/authHook.ts
@@ -1,13 +1,13 @@
-import { auth } from "@/firebase";
-import { onAuthStateChanged, User } from "firebase/auth";
-import { useEffect, useState } from "react";
+import { auth } from '@/firebase';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { useEffect, useState } from 'react';
 
 export function useUser() {
-    const [user, setUser] = useState<User | false | null>(false);
-    
-    useEffect(() => {
-        return onAuthStateChanged(auth, (user) => setUser(user))
-    }, []);
+  const [user, setUser] = useState<User | false | null>(false);
 
-    return user;
+  useEffect(() => {
+    return onAuthStateChanged(auth, (user) => setUser(user));
+  }, []);
+
+  return user;
 }
