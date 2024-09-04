5e7c558523eb33ad3ed8bd81a112fff03a4c1fb0
Viktoria Rashchepkina <vika.rashchepkina.04@bk.ru>
2024-09-04 12:54:09 +0500

    feat: add error while receiving data

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -8,6 +8,7 @@ import HistoryItem from '@/components/HistoryPageComponents/HistoryItem';
 import { Pagination, Stack, Typography } from '@mui/material';
 import { useRedirectToRequest } from '@/hooks/historyHook';
 import { useTranslations } from 'next-intl';
+import Loading from '@/app/loading';
 
 const KEY_RESTFUL = 'RESTFul-store';
 const KEY_GRAPHQL = 'graphqlRequests';
@@ -16,19 +17,33 @@ const ITEMS_PER_PAGE = 10;
 export default function HistoryPage() {
   const [requests, setRequests] = useState<(GraphqlRequest | RESTFulState)[]>([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [isEmpty, setIsEmpty] = useState(false);
+
   const redirectToRequest = useRedirectToRequest();
   const t = useTranslations('HistoryPage');
 
   useEffect(() => {
-    const restRequests = getArrayFromLocalStorage(KEY_RESTFUL);
-    const graphqlRequests = getArrayFromLocalStorage(KEY_GRAPHQL);
+    try {
+      const restRequests = getArrayFromLocalStorage(KEY_RESTFUL);
+      const graphqlRequests = getArrayFromLocalStorage(KEY_GRAPHQL);
 
-    const combinedRequests = [...restRequests, ...graphqlRequests].sort(
-      (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
-    );
+      const combinedRequests = [...restRequests, ...graphqlRequests].sort(
+        (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime(),
+      );
 
-    setRequests(combinedRequests);
-  }, []);
+      if (combinedRequests.length === 0) {
+        setIsEmpty(true);
+      } else {
+        setRequests(combinedRequests);
+      }
+    } catch (err) {
+      setError(t('ErrorLC'));
+    } finally {
+      setIsLoading(false);
+    }
+  }, [t]);
 
   const handleRequestClick = (request: GraphqlRequest | RESTFulState) => {
     redirectToRequest(request);
@@ -47,8 +62,13 @@ export default function HistoryPage() {
   return (
     <div className={historyStyles['history-page']}>
       <h2>{t('HistoryPageTitle')}</h2>
-      {requests.length === 0 && <EmptyHistoryNotification t={t} />}
-      {requests.length > 0 && (
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <div>{error}</div>
+      ) : isEmpty ? (
+        <EmptyHistoryNotification t={t} />
+      ) : (
         <div className={historyStyles['history-page__requests-container']}>
           <Typography>
             {t('HistoryPaginationTitle')} {currentPage}
